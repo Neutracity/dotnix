6463e9127ff084c3d06a8ac9d0db38c46b189705
Mathurin <mathurin.proy@epita.fr>
2024-10-23 22:49:14 +0200

    askka

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -94,18 +94,15 @@
         # }
       ];
 
+
       extraLuaConfig = ''
         ${builtins.readFile ./nvim/options.lua}
+        ${builtins.readFile ./nvim/plugin/lsp.lua}
+        ${builtins.readFile ./nvim/plugin/cmp.lua}
+        ${builtins.readFile ./nvim/plugin/telescope.lua}
+        ${builtins.readFile ./nvim/plugin/treesitter.lua}
+        ${builtins.readFile ./nvim/plugin/other.lua}
       '';
-
-      # extraLuaConfig = ''
-      #   ${builtins.readFile ./nvim/options.lua}
-      #   ${builtins.readFile ./nvim/plugin/lsp.lua}
-      #   ${builtins.readFile ./nvim/plugin/cmp.lua}
-      #   ${builtins.readFile ./nvim/plugin/telescope.lua}
-      #   ${builtins.readFile ./nvim/plugin/treesitter.lua}
-      #   ${builtins.readFile ./nvim/plugin/other.lua}
-      # '';
     };
 
 }
