c02bb3c56413d8bf16ebd4ce0fec47b74b9810ea
Mathurin <mathurin.proy@epita.fr>
2024-10-25 20:18:41 +0200

    asdasdasd

--- home/home.nix
+++ home/home.nix
@@ -4,6 +4,7 @@
   imports = [
     ./programs
     ./src/script
+    ../stylix.nix
   ];
 
   home.username = "neutra";
@@ -20,20 +21,8 @@
     EDITOR = "nvim";
   };
 
-  #Stylix config
-
-  stylix.enable = true;
-  stylix.polarity = "dark";
-  stylix.image = ./src/wallpaper/evangelion_1.jpg;
-  #stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
-  stylix.fonts = {
-    serif = {
-      package = pkgs.nerdfonts.override { fonts = [ "Hack" ]; };
-      name = "Hack";
-    };
-
-  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
+
--- stylix.nix
+++ stylix.nix
@@ -1,7 +1,15 @@
 { config, pkgs, inputs, ... }:
 {
-  stylix.enable = true;
-  #stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
-  stylix.polarity = "dark";
-  stylix.image = ./home/src/wallpaper/evangelion_1.jpg;
+  stylix= {
+    enable = true;
+    #stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+    polarity = "dark";
+    image = ./home/src/wallpaper/evangelion_1.jpg;
+    fonts = {
+      serif = {
+        package = pkgs.nerdfonts.override { fonts = [ "Hack" ]; };
+        name = "Hack";
+      };
+    };
+  };
 }
