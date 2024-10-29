97fd10303fca813c018dc8c08e1e0022fb6c76cc
Mathurin <mathurin.proy@epita.fr>
2024-10-29 20:08:14 +0100

    iojadsojidas

--- home/programs/kitty/default.nix
+++ home/programs/kitty/default.nix
@@ -7,13 +7,12 @@
     #font.name = "Hack" ;
     #font.name = "jetbrains mono nerd font" ;
     settings = {
-      confirm_os_window_close= -1;
+      confirm_os_window_close= 0;
       allow_remote_control = true;
       close_on_child_death = true;
       cursor_shape = "beam";
       enable_audio_bell = false;
       shell = "zsh";
-      background_opacity = 0;
     };
     shellIntegration.enableZshIntegration = true;
 
--- stylix.nix
+++ stylix.nix
@@ -4,12 +4,13 @@
     enable = true;
     #stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
     polarity = "dark";
-    image = ./home/src/wallpaper/evangelion_1.jpg;
+    image = ./home/src/wallpaper/evangelion_2.jpg;
     fonts = {
       serif = {
         package = pkgs.nerdfonts.override { fonts = [ "Hack" ]; };
         name = "Hack";
       };
     };
+    opacity.terminal = 0.85;
   };
 }
