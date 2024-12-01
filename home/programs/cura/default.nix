2bbcb777842fe5d903cf306f9350942a39414673
Mathurin <mathurin.proy@epita.fr>
2024-12-01 11:13:12 +0100

    oakdfk

--- home/programs/cura/default.nix
+++ home/programs/cura/default.nix
@@ -1,20 +1,21 @@
 {
   lib,
-  fetchurl,
   makeDesktopItem,
-appimageTools,
 }:
+{
 
-appimageTools.wrapType2 rec {
-  pname = "cura";
-  version = "5.7.1";
-
-  src = fetchurl {
-    url = "https://github.com/Ultimaker/Cura/releases/download/${version}/UltiMaker-Cura-${version}-linux-X64.AppImage";
-    hash = "sha256-LZMD0fo8TSlDEJspvTka724lYq5EgrOlDkwMktXqATw=";
+  desktopItems =
+    let
+  meta = with lib; {
+    description = "Ultimaker Cura Printing Slicing Software";
+    homepage = "https://ultimaker.com/es/software/ultimaker-cura/";
+    changelog = "https://github.com/Ultimaker/Cura/releases/tag/${finalAttrs.version}";
+    license = licenses.lgpl3Plus;
+    platforms = platforms.linux;
+    mainProgram = "cura";
   };
-
-  desktopItems = [
+      
+    in [
     (makeDesktopItem {
       name = "cura";
       exec = "cura";
@@ -25,12 +26,4 @@ appimageTools.wrapType2 rec {
     })
   ];
 
-  meta = with lib; {
-    description = "Ultimaker Cura Printing Slicing Software";
-    homepage = "https://ultimaker.com/es/software/ultimaker-cura/";
-    changelog = "https://github.com/Ultimaker/Cura/releases/tag/${finalAttrs.version}";
-    license = licenses.lgpl3Plus;
-    platforms = platforms.linux;
-    mainProgram = "cura";
-  };
 }
