cde64095ce074c499c2c3c45b0d5f183ba721b06
Mathurin <mathurin.proy@epita.fr>
2024-12-15 20:58:57 +0100

    asdfadsf

---
+++ home/src/script/generalscript/default.nix
@@ -0,0 +1,37 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  better-nmtui = pkgs.writeShellScriptBin "better-nmtui" ''
+  NEWT_COLORS='
+	root=black,black
+	window=white,black
+	border=white,black
+	textbox=white,black
+	label=white,black
+	title=white,black
+	roottext=white,black
+	actbutton=white,black
+	fullscale=white,black
+	entry=white,black
+	actcheckbox=white,black
+	actlistbox=white,black
+	acttextbox=white,black
+	sellistbox=white,black
+	helpline=white,black
+	actsellistbox=white,darkgray
+	listbox=white,black
+	button=white,black
+	actbuttom=white,black
+	checkbox=white,black
+' ${pkgs.nmtui}
+
+  '';
+in {
+  home.packages = with pkgs; [
+    better-nmtui
+  ];
+}
+
